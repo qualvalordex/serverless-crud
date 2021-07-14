0cdff2c36f191e4f0ed7344049c7ccc5b528898f
Paulo Henrique Rodrigues <qualvalordex@gmail.com>
2021-07-14 19:41:48 -0300

    :sparkles: route to create users

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,17 +1,42 @@
+const db = require('../../models')
+const User = db.User
+
 async function create(event) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: "Criar usuários..."
-        })
+    const params = JSON.parse(event.body)
+    console.log(params)
+
+    try {
+        const result = await User.create(params)
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'User created successful.'
+            })
+        }
+    }
+    catch (error) {
+        console.error(error)
+        
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: error
+            })
+        }
     }
 }
 
 async function find(event) {
+    const userId = event.pathParameters.userId
+
+    const result = await User.findByPk(1)
+    console.log(result)
+
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: "Ver um usuário..."
+            user: result
         })
     }
 }
